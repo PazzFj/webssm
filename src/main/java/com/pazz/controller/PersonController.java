4aebe71afa14923d45c1dee9526cc1f79a7399ef
pazzfj <pazzfj@163.com>
2017-06-12 17:11:29 +0800

    update 2017年6月12日17:11:27

--- src/main/java/com/pazz/controller/PersonController.java
+++ src/main/java/com/pazz/controller/PersonController.java
@@ -7,30 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2017-4-8.
  */
-@Controller
+@RestController
+@RequestMapping("/perosn")
 public class PersonController {
 
     @Autowired
-    private IPersonDao personDao;
+    private PersonService personService;
 
     @ResponseBody
     @RequestMapping("/getPersons")
     public List<Person> getPersons(){
-        System.out.println("_____yttyt__");
-        List<Person> personList = new ArrayList<Person>();
-        personList = personDao.getPersons();
-        personList.add(new Person(1, "zhangs", 21));
-        personList.add(new Person(2, "zhangc", 22));
-        personList.add(new Person(3, "zhangf", 23));
+        List<Person> personList = personService.getPersons();
         return personList;
     }
-
 
 }
--- src/main/java/com/pazz/service/PersonService.java
+++ src/main/java/com/pazz/service/PersonService.java
@@ -1,10 +1,25 @@
 package com.pazz.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pazz.dao.IPersonDao;
+import com.pazz.entity.Person;
+
 /**
  * Created by Administrator on 2017-4-8.
  */
+@Service
 public class PersonService {
+	
+	@Autowired
+	private IPersonDao personDao;
 
-
+	public  List<Person> getPersons(){
+		 List<Person> persons = personDao.getPersons();
+		return persons;
+	}
 
 }
