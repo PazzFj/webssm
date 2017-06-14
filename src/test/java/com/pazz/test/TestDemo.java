c767e62d18486d39d22d4cd9192e6e8e27da9ef9
pazzfj <pazzfj@163.com>
2017-06-14 22:13:07 +0800

    add TestDemo.java

---
+++ src/test/java/com/pazz/test/TestDemo.java
@@ -0,0 +1,17 @@
+package com.pazz.test;
+
+import org.junit.Test;
+
+/**
+* @author pazz
+* @create 2017年6月14日 下午10:09:42 
+*/
+
+public class TestDemo {
+	
+	@Test
+	public void test(){
+		
+	}
+
+}
